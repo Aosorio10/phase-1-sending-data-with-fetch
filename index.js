aabe178360875b780918bd81d57dbb2025836534
Alejandro Osorio <avot3110@hotmail.com>
2023-05-12 11:39:26 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+function submitData(name, email) {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+  
+    const body = JSON.stringify({
+      name,
+      email
+    });
+  
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers,
+      body
+    })
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += newId;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
